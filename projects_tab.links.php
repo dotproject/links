4f8f0d8cebc067695fe895e502306f9b4eeefd73
cyberhorse <cyberhorse>
2004-09-17 15:59:51 +0000

    Automatic tab for projects module.

---
+++ projects_tab.links.php
@@ -0,0 +1,6 @@
+<?php /* PROJECTS $Id: vw_files.php,v 1.8 2004/08/06 05:22:47 cyberhorse Exp $ */
+GLOBAL $AppUI, $project_id, $deny, $canRead, $canEdit, $dPconfig;
+
+$showProject = false;
+require( dPgetConfig('root_dir') . '/modules/links/index_table.php' );
+?>
